c6511829e1900b09ac44931aa842c57617e898d6
John Albers <jalbers90@gmail.com>
2023-11-20 14:50:53 -0600

    p2p protocol, encoding, message

--- p2p/handshake.go
+++ p2p/handshake.go
@@ -1,5 +1,9 @@
 package p2p
 
+// ErrInvalidhandshake is returned if the handshake b/w the local
+// and remote node fails or could not be established.
+// var ErrInvalidHandshake = errors.New("invalid handshake")
+
 // HandshakeFunc is ... ?
 type HandshakeFunc func(any) error
 
